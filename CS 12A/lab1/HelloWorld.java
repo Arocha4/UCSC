15cc22187977dfc14aedfd6870bf8ddc0c1a836b
Antoine <antoinerocha97@gmail.com>
2019-08-30 18:10:18 -0700

    Add files via upload

---
+++ CS 12A/lab1/HelloWorld.java
@@ -0,0 +1,11 @@
+    // HelloWorld.java
+    // Antoine Rocha
+    // arocha4
+    // lab1
+    // print" Hello, my name is antoine"
+    class HelloWorld{
+	public static void main(String[] args) {
+	System.out.println("Hello, my name is Antoine") ;
+}
+} 
+ 
