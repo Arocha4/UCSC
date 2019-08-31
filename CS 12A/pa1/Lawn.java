7b8e0d8146e809b7322408a44ff29aaf660dfb82
Antoine <antoinerocha97@gmail.com>
2019-08-30 18:16:07 -0700

    Add files via upload

---
+++ CS 12A/pa1/Lawn.java
@@ -0,0 +1,51 @@
+// Lawn.java
+// Antoine Rocha
+// arocha4
+// pa1
+// program takes user input and calulates lawn area and mowtime
+
+import java.util.Scanner;
+
+class Lawn{
+
+   public static void main( String[] args ){
+
+      double lotheight, lotwidth, lotarea, area, housewidth, househeight;
+      double housearea, mowrate, houselot, width, height;
+      Scanner sc = new Scanner(System.in);
+
+      System.out.print("Enter the length and width of the lot, in feet: ");
+      lotwidth = sc.nextDouble();
+      lotheight = sc.nextDouble();
+      lotarea = lotwidth*lotheight;
+
+      System.out.print("Enter the length and width of the house, in feet: ");
+      
+      System.out.print("The lawn area is: ");
+      housewidth = sc.nextDouble();
+      househeight = sc.nextDouble();
+      housearea = housewidth*househeight;
+      area = lotarea-housearea; 
+      System.out.println(area);
+      
+      System.out.print("Enter the mowing rate, in square feet persecond: ");
+      mowrate = sc.nextDouble();
+      
+
+      double sec = area / mowrate;
+      int h, m,s,n;
+      
+      s = (int) Math.round(sec); // s = 3726
+      m = s/60;
+      s = s%60;  // same as s %= 60
+      h = m/60;
+      m = m%60;  // same as m %= 60
+
+    
+      System.out.print("The mowing time is ");
+      System.out.print(h + (h == 1 ? " hour, " : " hours, "));
+      System.out.print(m + (m == 1 ? " minute, " : " minutes, "));
+      System.out.println(s + (s == 1 ? " second. " : " seconds. "));
+   }
+
+}
